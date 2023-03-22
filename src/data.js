39e71da4e1c55ac8acca99f9f27f2c18bfe2b4e0
Bárbara Barrios Fulquet <barbarabarriosfulquet@gmail.com>
2023-03-22 10:34:36 -0300

    Agregamos estilos y modificamos estructura del HTML
    Tradujimos las descripciones de las películas
    Optimizamos las funciones asociadas a la toma de datos
    Agregamos una nueva función para el cálculo porcentual
    Modificamos CSS de la sección Películas.

--- src/data.js
+++ src/data.js
@@ -1,27 +1,19 @@
-import data from "./data/ghibli/ghibli.js";
-
 // FILTROS
 // Director
 export function filtroInformacionDirector(films, nombreDirector) {
   const resultadoDirector = films.filter((y) => y.director === nombreDirector);
   return resultadoDirector;
-};
+}
 // Productor
 export function filtroInformacionProductor(films, nombreProductor) {
   const resultadoProductor = films.filter((y) => y.producer === nombreProductor);
   return resultadoProductor;
-};
-// Especie
-export const listaPersonajes = [];
-for (let i = 0; i < data.films.length; i++) {
-  for (let j = 0; j < data.films[i].people.length; j++) {
-    listaPersonajes.push(data.films[i].people[j]);
-  }
 }
+// Especie
 export function filtroInformacionEspecie(listaPersonajes, tipoEspecie) {
   const resultadoEspecie = listaPersonajes.filter((y) => y.specie === tipoEspecie);
   return resultadoEspecie;
-};
+}
 
 // ORDENAMIENTO
 // Fecha de lanzamiento
@@ -53,7 +45,7 @@ export const ordenInformación = (order, films) => {
 export const calculoInformacion = (data, especie) => {
   let calculoEspecie = 0;
   for (let i = 0; i < data.length; i++) {
-    let personaje = data[i].people;
+    const personaje = data[i].people;
     for (let j = 0; j < personaje.length; j++) {
       if (personaje[j].specie === especie) {
         calculoEspecie++;
